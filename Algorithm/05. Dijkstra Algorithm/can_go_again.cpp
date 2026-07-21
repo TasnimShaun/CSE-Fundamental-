204e705224b0dd99cd5a35b002eba63c87ddc651
MD TASNIMUL HASAN <tasnimshaun3@gmail.com>
2026-07-21 13:08:50 +0800

    Add files via upload

---
+++ Algorithm/05. Dijkstra Algorithm/can_go_again.cpp
@@ -0,0 +1,86 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Edge
+{
+    public:
+         int a,b,c;
+         Edge(int a,int b, int c)
+         {
+            this->a = a;
+            this->b = b;
+            this->c = c;
+         }
+
+};
+int dis[1005];
+vector<Edge> edge_list;
+int n,e;
+void bellman_ford(int source)
+{
+    for(int i=0;i<n-1;i++)
+    {
+        for(auto ed : edge_list)
+        {
+            int a,b,c;
+            a = ed.a;
+            b = ed.b;
+            c = ed.c;
+
+            if(dis[a]!= INT_MAX && dis[a] + c<dis[b])
+             dis[b] = dis[a]+c;
+        }
+    }
+    bool cycle = false;
+    for(auto ed : edge_list)
+    {
+        int a,b,c;
+        a = ed.a;
+        b = ed.b;
+        c = ed.c;
+
+        if(dis[a]!= INT_MAX && dis[a] + c<dis[b])
+         {
+            cycle = true;
+            break;
+         }
+    }
+    if(cycle)
+    {
+         cout << "Negative Cycle Detected";
+         
+    }
+    else{
+        int t; 
+    cin >> t;
+    while(t--){
+        int destination;
+        cin >> destination;
+        if(dis[destination] == LLONG_MAX)
+          cout << "Not Possible" << endl;
+          else
+          cout << dis[destination] <<endl;
+    }
+
+}
+}
+
+int main()
+{
+    cin >> n >> e;
+    while(e--)
+    {
+        int a,b,c;
+        cin >> a >>b >>c;
+        edge_list.push_back(Edge(a,b,c));
+    }
+    for(int i =0;i<n;i++){
+      dis[i] = INT_MAX;
+    }
+     int source;
+     cin >> source;
+    dis[source]= 0;
+
+    bellman_ford(source);
+
+    return 0;
+}
