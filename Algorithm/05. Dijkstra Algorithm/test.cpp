5ae5e4578a457349ee1c9ff08da24788d371331a
MD TASNIMUL HASAN <tasnimshaun3@gmail.com>
2026-07-21 13:13:16 +0800

    Add files via upload

---
+++ Algorithm/05. Dijkstra Algorithm/test.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<pair<int,int>>adj_list[105];
+ int dis[105];
+void dijkstra(int src)
+{
+  priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+  pq.push({0,src});
+  dis[src] = 0;
+  while(!pq.empty())
+  {
+     pair<int,int> pa_ir = pq.top();
+     pq.pop(); 
+     int parent_node = pa_ir.second;
+     int parent_dis  = pa_ir.first;
+
+     for( auto child : adj_list[parent_node]) // use auto 
+        {
+           int child_node = child.first;
+           int child_dis = child.second;
+           if(parent_dis + child_dis < dis[child_node])
+           {
+             dis[child_node] = parent_dis + child_dis;
+             pq.push({dis[child_node],child_node});
+           }
+       }
+
+   }
+}
+
+int main()
+{
+    int n,e;
+    cin >> n >> e;
+    while(e--)
+    {
+    int a,b,c;
+    cin >> a >> b >> c;
+    adj_list[a].push_back({b,c});
+    adj_list[b].push_back({a,c});
+    }
+    for(int i=1;i<n;i++)
+      dis[i] = INT_MAX;
+
+    dijkstra(0);
+
+    for(int i =0;i<n;i++)
+    {
+     cout << i << "-> " <<dis[i] << endl;
+    }
+    return 0;
+}
