83e213e3d17088853bc5543fc02e65028ed7c8d9
MD TASNIMUL HASAN <tasnimshaun3@gmail.com>
2026-07-21 13:09:24 +0800

    Add files via upload

---
+++ Algorithm/05. Dijkstra Algorithm/Dijkstra_Naive.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<pair<int,int>>adj_list[105];
+ int dis[105];
+void dijkstra(int src)
+{
+  queue<pair<int,int>> q;
+  q.push({src,0});
+  dis[src] = 0;
+  while(!q.empty())
+  {
+     pair<int,int> pa_ir = q.front();
+     q.pop(); 
+     int parent_node = pa_ir.first;
+     int parent_dis  = pa_ir.second;
+
+     for( auto child : adj_list[parent_node]) // use auto 
+        {
+           int child_node = child.first;
+           int child_dis = child.second;
+           if(parent_dis + child_dis < dis[child_node])
+           {
+             dis[child_node] = parent_dis + child_dis;
+             q.push({child_node,dis[child_node]});
+           }
+       }
+
+   }
+}
+
+int main()
+{
+    int n,e;
+    cin >> n >> e;
+    while(e--)
+    {
+    int a,b,c;
+    cin >> a >> b >> c;
+    adj_list[a].push_back({b,c});
+    adj_list[b].push_back({a,c});
+    }
+    for(int i=0;i<n;i++)
+      dis[i] = INT_MAX;
+
+    dijkstra(0);
+
+    for(int i =0;i<n;i++)
+    {
+     cout << i << "-> " <<dis[i] << endl;
+    }
+    return 0;
+}
