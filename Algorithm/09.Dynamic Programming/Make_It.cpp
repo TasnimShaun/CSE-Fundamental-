6e9ddde380a0612a56a7b0dec15ef99c94d59f70
MD TASNIMUL HASAN <tasnimshaun3@gmail.com>
2026-07-23 21:35:42 +0800

    Add files via upload

---
+++ Algorithm/09.Dynamic Programming/Make_It.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+      int n;
+      cin >> n;
+       if(n == 1)
+         cout <<"YES" << endl;
+         else
+         {
+            if( 1 + 3 || 1*2 == n)
+             cout << "YES" <<endl;
+             else
+             cout << "NO" <<endl;
+         }
+         
+    }
+    return 0;
+}
